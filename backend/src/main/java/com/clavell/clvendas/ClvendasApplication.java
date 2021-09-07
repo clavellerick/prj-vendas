e0aaed41a9213e8c16baca0bff30d75ec2172244
Erick Clavell <clavellerick@gmail.com>
2021-09-07 17:22:07 -0300

    Project created

---
+++ backend/src/main/java/com/clavell/clvendas/ClvendasApplication.java
@@ -0,0 +1,13 @@
+package com.clavell.clvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClvendasApplication.class, args);
+	}
+
+}
